adf80166dc7539ce1cad1857ed39e3218962fd63
Hamid Bourja <bourjamthb@gmail.com>
2023-09-22 20:18:55 +0100

    feat: add `base-alert`

---
+++ src/components/BaseAlert.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container position-absolute">
+    <div
+      :class="`alert alert-${type}`"
+      role="alert">
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    type: {
+      type: String,
+      default: 'success',
+    },
+    message: String,
+  });
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    top: 0.5rem;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+</style>
