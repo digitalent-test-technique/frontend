6a8a24d16284fc760b932bfb284d92ba92f51348
Hamid Bourja <bourjamthb@gmail.com>
2023-09-22 20:18:22 +0100

    feat: add `base-input`

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <label class="form-group">
+    {{ label }}
+    <input
+      v-model="value"
+      class="form-control"
+      :type="type"
+      :placeholder="placeholder" />
+  </label>
+</template>
+
+<script setup>
+  import { computed } from 'vue';
+
+  const props = defineProps({
+    modelValue: String,
+    type: {
+      type: String,
+      default: 'text',
+    },
+    label: String,
+    placeholder: String,
+  });
+
+  const emit = defineEmits(['update:modelValue']);
+
+  const value = computed({
+    get() {
+      return props.modelValue;
+    },
+    set(value) {
+      emit('update:modelValue', value);
+    },
+  });
+</script>
+
+<style lang="scss"></style>
