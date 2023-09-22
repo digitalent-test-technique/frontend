47234525fe1fb8fdecde4f92642bad456e6b93e8
Hamid Bourja <bourjamthb@gmail.com>
2023-09-22 20:16:54 +0100

    feat: defind vite server port feat: defind vite server port & aliases aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
   plugins: [vue()],
+  server: {
+    port: 3000,
+  },
 });
