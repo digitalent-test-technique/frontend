539bab2cb5b9bc44650417c681ee7ef97a2eee84
Hamid Bourja <bourjamthb@gmail.com>
2023-09-25 14:33:15 +0100

    feat: add steps to signup

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -5,54 +5,56 @@
     type="danger">
   </base-alert>
   <form
+    ref="form"
     class="container p-5 mt-5 needs-validation"
     :class="{ 'was-validated': isSubmitted }"
     novalidate
-    @submit.prevent="handleSubmit">
-    <div class="row">
-      <base-input
-        v-model="lastName"
-        class="col-md-6"
-        label="Nom"
-        placeholder="Nom">
-      </base-input>
-      <base-input
-        v-model="firstName"
-        class="col-md-6"
-        label="Prénom"
-        placeholder="Prénom">
-      </base-input>
-    </div>
-    <div class="row">
+    @submit.prevent.stop="handleSubmit">
+    <template v-if="isStepOne">
+      <div class="row">
+        <base-input
+          v-model="lastName"
+          class="col-md-6"
+          label="Nom"
+          placeholder="Nom">
+        </base-input>
+        <base-input
+          v-model="firstName"
+          class="col-md-6"
+          label="Prénom"
+          placeholder="Prénom">
+        </base-input>
+      </div>
+      <div class="row">
+        <base-input
+          v-model="phone"
+          class="col-md-6"
+          label="Téléphone"
+          type="tel"
+          placeholder="Téléphone">
+        </base-input>
+        <base-input
+          v-model="address"
+          class="col-md-6"
+          label="Adresse"
+          placeholder="Adresse">
+        </base-input>
+      </div>
+    </template>
+
+    <template v-else>
       <base-input
         v-model="email"
-        class="col-md-6"
         label="E-mail"
         type="email"
         placeholder="Adresse e-mail">
       </base-input>
       <base-input
-        v-model="address"
-        class="col-md-6"
-        label="Adresse"
-        placeholder="Adresse">
-      </base-input>
-    </div>
-    <div class="row">
-      <base-input
-        v-model="phone"
-        class="col-md-6"
-        label="Téléphone"
-        type="tel"
-        placeholder="Téléphone">
-      </base-input>
-      <base-input
         v-model="password"
-        class="col-md-6"
         label="Mot de passe"
         type="password"
         placeholder="Mot de passe"></base-input>
-    </div>
+    </template>
     <div
       v-if="!isAuth"
       class="form-group">
@@ -63,14 +65,20 @@
         Connectez-vous</router-link
       >
     </div>
-    <div class="form-group">
-      <btn-primary class="ml-auto">
+    <div class="form-group d-flex">
+      <btn-primary class="mr-auto">
         <span
           v-if="isLoading"
           class="spinner-border spinner-border-sm"
           role="status"
           aria-hidden="true"></span>
-        inscrire
+        {{ isStepOne ? "L'étape suivante" : 'inscrire' }}
+      </btn-primary>
+      <btn-primary
+        v-if="!isStepOne"
+        type="button"
+        @click="isStepOne = true">
+        L'étape précédente
       </btn-primary>
     </div>
   </form>
@@ -83,6 +91,10 @@
   import BtnPrimary from '@/components/BtnPrimary.vue';
   import { useUserStore } from '@/store/user';
   import BaseAlert from '@/components/BaseAlert.vue';
+
+  const form = ref();
+
+  const isStepOne = ref(true);
 
   const isSubmitted = ref(false);
 
@@ -102,28 +114,37 @@
   const { signUp } = userStore;
 
   async function handleSubmit() {
-    try {
-      isLoading.value = true;
-      errorMessage.value = null;
-      isSubmitted.value = true;
-      const response = await signUp({
-        lastName: lastName.value,
-        firstName: firstName.value,
-        email: email.value,
-        address: address.value,
-        phone: phone.value,
-        password: password.value,
-      });
-      if (!response.ok) {
-        errorMessage.value = response.message;
+    const invalidInputs = form.value.querySelectorAll(':invalid');
+    isSubmitted.value = true;
+    if (invalidInputs.length === 0) {
+      if (isStepOne.value) {
+        isStepOne.value = false;
+        isSubmitted.value = false;
+      } else {
+        try {
+          isLoading.value = true;
+          errorMessage.value = null;
+
+          const response = await signUp({
+            lastName: lastName.value,
+            firstName: firstName.value,
+            email: email.value,
+            address: address.value,
+            phone: phone.value,
+            password: password.value,
+          });
+          if (!response.ok) {
+            errorMessage.value = response.message;
+          }
+        } catch (er) {
+          console.log(er);
+        } finally {
+          isLoading.value = false;
+          setTimeout(() => {
+            errorMessage.value = null;
+          }, 2000);
+        }
       }
-    } catch (er) {
-      console.log(er);
-    } finally {
-      isLoading.value = false;
-      setTimeout(() => {
-        errorMessage.value = null;
-      }, 2000);
     }
   }
 </script>
